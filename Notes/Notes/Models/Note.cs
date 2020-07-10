a650068d8957bc8f6650bc158d617a59754e18f1
David Vera <David Vera@PERSONAL>
2020-07-09 21:18:09 -0500

    Project done!!!!

--- Notes/Notes/Models/Note.cs
+++ Notes/Notes/Models/Note.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using SQLite;
 
 namespace Notes.Models
 {
     public class Note
     {
-        public string Filename { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
     }
 }
-
